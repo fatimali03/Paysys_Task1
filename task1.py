20991c0f2ac067a04c88e7151f1b5d455130437a
fatimali03 <136559675+fatimali03@users.noreply.github.com>
2024-06-06 13:19:13 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,8 +3,6 @@ def calculate_output(a, b):
     if a <= 0 or b <= 0:
         return "Error"
     sum_of_numbers = a + b
-    #doing an example usage for test purpose with a new line of code added to orignal file
-    sum_of_numbers = sum_of_numbers - 20
     if sum_of_numbers > 100:
         return sum_of_numbers
     elif sum_of_numbers == 100:
