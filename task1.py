30a262027be8fc0e93ca3fddc3c01b3957fc0699
fatimali03 <136559675+fatimali03@users.noreply.github.com>
2024-06-06 13:05:41 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,6 +3,7 @@ def calculate_output(a, b):
     if a <= 0 or b <= 0:
         return "Error"
     sum_of_numbers = a + b
+  
     if sum_of_numbers > 100:
         return sum_of_numbers
     elif sum_of_numbers == 100:
